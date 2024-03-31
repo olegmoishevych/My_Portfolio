cb265649097b584ab4337d5f570f88f1faf85dcb
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-03-31 17:26:46 +0300

    change

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -5,31 +5,30 @@ import telegram from '../assets/image/telegram.png'
 import gmail from '../assets/image/gmail.png'
 import {Title} from "../common/components/title/Title";
 
-
 export const Footer = () => {
-    return (
-        <div className={style.footer}>
-            <div className={style.container}>
-                <Title text={'Ekaterina Yurchyk'}/>
-                <div className={style.socialIcons}>
-                    <div className={style.socialIcon}>
-                        <a href={'https://github.com/EkaterinaYurchyk2022'} target={'_blank'}>
-                            <img src={GitHub} alt=''/>
-                        </a>
-                    </div>
-                    <div className={style.socialIcon}>
-                        <a href={'https://t.me/EkaterinaYurchyk'}>
-                            <img src={telegram} alt=''/>
-                        </a>
-                    </div>
-                    <div className={style.socialIcon}>
-                        <a href={'mailto:ekaterina.yurchyk.s@gmail.com'}>
-                            <img src={gmail} alt=''/>
-                        </a>
-                    </div>
-                </div>
-                <span className={style.copyright}>© 2022 All Rights Reserved</span>
-            </div>
+  return (
+    <div className={style.footer}>
+      <div className={style.container}>
+        <Title text={'Ekaterina Yurchyk'}/>
+        <div className={style.socialIcons}>
+          <div className={style.socialIcon}>
+            <a href={'https://github.com/EkaterinaYurchyk2022'} target={'_blank'} rel="noreferrer">
+              <img src={GitHub} alt='GitHub'/>
+            </a>
+          </div>
+          <div className={style.socialIcon}>
+            <a href={'https://t.me/EkaterinaYurchyk'} target={'_blank'} rel="noreferrer">
+              <img src={telegram} alt='Telegram'/>
+            </a>
+          </div>
+          <div className={style.socialIcon}>
+            <a href={'mailto:ekaterina.yurchyk.s@gmail.com'} target={'_blank'} rel="noreferrer">
+              <img src={gmail} alt='Gmail'/>
+            </a>
+          </div>
         </div>
-    )
+        <span className={style.copyright}>© 2022 All Rights Reserved</span>
+      </div>
+    </div>
+  )
 }
--- src/header/burgerNav/BurgerNav.tsx
+++ src/header/burgerNav/BurgerNav.tsx
@@ -1,51 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import style from './BurgerNav.module.scss';
-import {Link, animateScroll as scroll} from 'react-scroll';
-
+import { Link } from 'react-scroll';
 
 export const BurgerNav = () => {
-    let [openedMenu, setOpenedMenu] = useState(false)
+  let [openedMenu, setOpenedMenu] = useState(false);
 
-    let onBurgerBtnClick = () => {
-        setOpenedMenu(!openedMenu)
-        console.log(openedMenu)
-    }
-    return (
-        <div className={style.burgerNav}>
-            <div className={openedMenu ? `${style.burgerNavItems} ${style.show}` : style.burgerNavItems}>
-                < Link
-                    activeClass={style.active}
-                    to='main'
-                    spy={true}
-                    smooth={true}
-                    offset={1}
-                    duration={500}>Main</Link>
-                <Link
-                    activeClass={style.active}
-                    to='skills'
-                    spy={true}
-                    smooth={true}
-                    offset={1}
-                    duration={500}>Skills</Link>
-                <Link
-                    activeClass={style.active}
-                    to='projects'
-                    spy={true}
-                    smooth={true}
-                    offset={1}
-                    duration={500}>Projects</Link>
-                <Link
-                    activeClass={style.active}
-                    to='contacts'
-                    spy={true}
-                    smooth={true}
-                    offset={1}
-                    duration={500}>Contacts</Link>
-            </div>
-            <div className={style.burgerBtn} onClick={onBurgerBtnClick}>
+  let onBurgerBtnClick = () => {
+    setOpenedMenu(!openedMenu);
+    console.log(openedMenu);
+  };
 
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.burgerNav}>
+      <div className={openedMenu ? `${style.burgerNavItems} ${style.show}` : style.burgerNavItems}>
+        <Link
+          activeClass={style.active}
+          to='main'
+          spy={true}
+          smooth={true}
+          offset={1}
+          duration={500}>Main</Link>
+        <Link
+          activeClass={style.active}
+          to='skills'
+          spy={true}
+          smooth={true}
+          offset={1}
+          duration={500}>Skills</Link>
+        <Link
+          activeClass={style.active}
+          to='projects'
+          spy={true}
+          smooth={true}
+          offset={1}
+          duration={500}>Projects</Link>
+        <Link
+          activeClass={style.active}
+          to='contacts'
+          spy={true}
+          smooth={true}
+          offset={1}
+          duration={500}>Contacts</Link>
+      </div>
+      <div className={style.burgerBtn} onClick={onBurgerBtnClick}></div>
+    </div>
+  );
 }
-
