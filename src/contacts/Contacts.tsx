2117a363f6a03b21ba8a18f142a7eee9ad4f50e9
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-04-12 23:04:32 +0300

    change contacts

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,50 +1,74 @@
 import React from 'react';
-import style from './Contacts.module.scss'
-import {Title} from '../common/components/title/Title';
-import {useForm} from 'react-hook-form';
-import {Button} from '../common/components/button/Button';
-import axios from 'axios';
-
-
-const Fade = require('react-reveal/Fade')
+import style from './Contacts.module.scss';
+import { useForm } from 'react-hook-form';
+import { Title } from '../common/components/title/Title';
+import { Button } from '../common/components/button/Button';
+const Fade = require('react-reveal/Fade');
 
 interface IFormInputs {
-    name: string
-    email: string
-    message: string
+  name: string;
+  email: string;
+  message: string;
 }
 
 export const Contacts = () => {
-    const {register, formState: {errors}, handleSubmit} = useForm<IFormInputs>();
-    const onSubmit = (data: IFormInputs, e: any) => {
-        e.preventDefault()
-        axios.post('https://gmail-smtp-nodejs-my-server.herokuapp.com/', {data})
-            .then((res) => {
-                alert('Thank you for your message');
-            });
-        e.target.reset()
-    }
-    return (
-        <div id='contacts' className={style.contactsBlock}>
-            <div className={style.container}>
-                <Fade top>
-                <Title text={'Contacts'}/>
-                </Fade>
-                    <Fade>
-                    <form className={style.form} onSubmit={handleSubmit(onSubmit)} id={'contact-form'}>
-                        <input placeholder={'Your name'} type={'text'} className={style.formArea} {...register('name', {required: true})}/>
-                        {errors.name && <p style={{color: '#fff'}}>Name is required</p>}
-                        <input {...register('email', {
-                            required: true,
-                            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-                        })} placeholder={'E-mail'} type={'text'} className={style.formArea}/>
-                        {errors.email && <p style={{color: '#fff'}}>E-mail is required</p>}
-                        <textarea {...register('message', {required: true})} placeholder={'Your message'} className={style.messageArea}/>
-                        <Button type='submit' buttonTitle={'Send message'}/>
-                    </form>
-                </Fade>
+  const { register, formState: { errors }, handleSubmit } = useForm<IFormInputs>();
 
-            </div>
-        </div>
-    );
+  const sendTelegramMessage = async (data: IFormInputs) => {
+    const TELEGRAM_API = 'https://api.telegram.org';
+    const ACCESS_TOKEN = '6690057162:AAEDTusfCHZ0rJw-s12Cga03njJxvKhaMCM';
+    const CHAT_ID = '@Portfolio_father_bot';
+    const text = `Name: ${data.name}\nEmail: ${data.email}\nMessage: ${data.message}`;
+
+    const response = await fetch(`${TELEGRAM_API}/bot${ACCESS_TOKEN}/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: text
+      })
+    });
+
+    return response.json();
+  };
+
+  const onSubmit = (data: IFormInputs, e: any) => {
+    e.preventDefault();
+    sendTelegramMessage(data).then(response => {
+      console.log('Message sent:', response);
+      alert('Thank you for your message');
+    }).catch(error => {
+      console.error('Error sending message:', error);
+      alert('Failed to send message');
+    });
+    e.target.reset();
+  };
+
+  return (
+    <div id='contacts' className={style.contactsBlock}>
+      <div className={style.container}>
+        <Fade top>
+          <Title text={'Contacts'} />
+        </Fade>
+        <Fade>
+          <form className={style.form} onSubmit={handleSubmit(onSubmit)} id={'contact-form'}>
+            <input placeholder={'Your name'} type={'text'} className={style.formArea} {...register('name', { required: true })} />
+            {errors.name && <p style={{ color: '#fff' }}>Name is required</p>}
+            <input {...register('email', {
+              required: "E-mail is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Invalid email address"
+              }
+            })} placeholder={'E-mail'} type={'text'} className={style.formArea} />
+            {errors.email && <p style={{ color: '#fff' }}>{errors.email.message}</p>}
+            <textarea {...register('message', { required: true })} placeholder={'Your message'} className={style.messageArea} />
+            <Button type='submit' buttonTitle={'Send message'} />
+          </form>
+        </Fade>
+      </div>
+    </div>
+  );
 };
