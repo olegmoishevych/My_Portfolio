b511fc3263ad8ac1e2bac2d1668ad2a9e372092a
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-04-14 22:31:55 +0300

    change contacts

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -5,6 +5,7 @@ import { Title } from '../common/components/title/Title';
 import { Button } from '../common/components/button/Button';
 const Fade = require('react-reveal/Fade');
 
+
 interface IFormInputs {
   name: string;
   email: string;
